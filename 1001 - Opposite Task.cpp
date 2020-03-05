794b0fbc909a1862afeb6139e54f077a9f54ef31
Solaiman Hossain <61455333+solaimanhs25@users.noreply.github.com>
2020-03-05 12:45:44 +0600

    Create 1001 - Opposite Task.cpp

---
+++ 1001 - Opposite Task.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--){
+        int noOfPrblm;
+        cin>>noOfPrblm;
+
+        cout<<noOfPrblm/2<<" ";
+        cout<<noOfPrblm-noOfPrblm/2<<endl;
+
+    }
+
+    return 0;
+}
+
